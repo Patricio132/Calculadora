5166791736883974f2aaa1f852d8cfca2c4d33ad
JAMSPOLL132 <mrsnipero132@gmail.com>
2021-05-22 16:13:24 -0500

    Calculadora

---
+++ Calculadora.js
@@ -0,0 +1,18 @@
+class Calculadora {
+    restar(num1, num2) {
+        return num1 - num2;
+    }
+
+    sumar(num1, num2) {
+        return num1 + num2;
+    }
+    
+    multiplicar(num1, num2) {
+        return num1 * num2;
+    }
+
+    dividir(num1, num2) {
+        return num1 / num2;
+    }
+
+}
